e08dfc33205b1e24479c5c2740d3217bd094559b
jorge garcia <jorge.garcia.celis@gmail.com>
2024-11-07 09:12:02 -0600

    .

--- content-script.js
+++ content-script.js
@@ -11,20 +11,22 @@ window.onload = () => {
         "#main > div > ion-content > ion-grid > ion-row > ion-col > ion-card:nth-child(1) > ion-card-content > ion-button"
     );
 
-    username.value = "jgarcia";
-    password.value = "Lacosa05.";
+    if (!!username && !!password && !!loginButton) {
+        username.value = "jgarcia";
+        password.value = "Lacosa05.";
 
-    username.dispatchEvent(new Event("input", { bubbles: true }));
-    password.dispatchEvent(new Event("input", { bubbles: true }));
+        username.dispatchEvent(new Event("input", { bubbles: true }));
+        password.dispatchEvent(new Event("input", { bubbles: true }));
 
-    function login() {
-        loginButton.dispatchEvent(new Event("click", { bubbles: true }));
+        function login() {
+            loginButton.dispatchEvent(new Event("click", { bubbles: true }));
+        }
+        setTimeout(login, 100);
     }
-    setTimeout(login, 100);
 
     let tutorial = document.querySelector(
         "#main > div > ion-content > ion-grid > ion-row > ion-col > ion-card:nth-child(2)"
     );
 
-    tutorial.style.display = "none";
+    if (!!tutorial) tutorial.style.display = "none";
 };
