a97129ff8061c78f6a672f3797ceb5e3015c39c4
jorge garcia <jorge.garcia.celis@gmail.com>
2024-11-07 10:55:20 -0600

    Remove tutorial, login works

--- content-script.js
+++ content-script.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+    // Selección de los elementos de login
     let username = document.querySelector(
         "#main > div > ion-content > ion-grid > ion-row > ion-col > ion-card:nth-child(1) > ion-card-content > ion-list > ion-item:nth-child(1) > ion-input"
     );
@@ -11,7 +12,8 @@ window.onload = () => {
         "#main > div > ion-content > ion-grid > ion-row > ion-col > ion-card:nth-child(1) > ion-card-content > ion-button"
     );
 
-    if (!!username && !!password && !!loginButton) {
+    // Si los elementos existen, asigna los valores y haz clic en el botón de login
+    if (username && password && loginButton) {
         username.value = "jgarcia";
         password.value = "Lacosa05.";
 
@@ -21,12 +23,21 @@ window.onload = () => {
         function login() {
             loginButton.dispatchEvent(new Event("click", { bubbles: true }));
         }
+
         setTimeout(login, 100);
     }
 
-    let tutorial = document.querySelector(
-        "#main > div > ion-content > ion-grid > ion-row > ion-col > ion-card:nth-child(2)"
-    );
+    let tutorial = null;
 
-    if (!!tutorial) tutorial.style.display = "none";
+    setInterval(() => {
+        try {
+            tutorial = document.querySelector(
+                "#main > div > ion-content > ion-grid > ion-row > ion-col > ion-card:nth-child(2)"
+            );
+
+            tutorial.setAttribute("style", "display: none;");
+        } catch (error) {
+            console.log(error);
+        }
+    }, 50);
 };
